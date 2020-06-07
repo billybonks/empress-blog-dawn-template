16be8a1f5f79d9172d0e3ec3893b64c61bd27d01
billybonks <sebastienstettler@gmail.com>
2020-06-07 23:02:20 +0800

    try fix component proxy

---
+++ addon/components/article-feed.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/article-feed';
+
+export default Component.extend({
+  layout
+});
---
+++ addon/components/featured-articles.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/featured-articles';
+
+export default Component.extend({
+  layout
+});
